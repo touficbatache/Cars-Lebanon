0c08d96590c8ebcf378005c6228dfa21c9c76483
touficbatache <taffou2a@gmail.com>
2019-10-29 12:27:48 +0200

    Enhancement: better UI Fix: rewards no longer working: creating a new account every 5 searches

--- app/src/main/java/com/batache/cars/MainActivity.java
+++ app/src/main/java/com/batache/cars/MainActivity.java
@@ -1,28 +1,23 @@
 package com.batache.cars;
 
 import android.content.Context;
-import android.content.DialogInterface;
 import android.os.AsyncTask;
 import android.os.Bundle;
-import android.text.Editable;
-import android.text.TextWatcher;
 import android.view.MenuItem;
 import android.view.View;
 import android.webkit.WebView;
-import android.widget.FrameLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatEditText;
+import androidx.appcompat.widget.AppCompatTextView;
 import androidx.appcompat.widget.PopupMenu;
 
 import com.batache.cars.Utils.GeneralUtils;
 import com.batache.cars.asynctask.HttpServiceHandler;
 import com.google.android.material.button.MaterialButton;
 import com.google.android.material.dialog.MaterialAlertDialogBuilder;
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.textfield.TextInputEditText;
 
 import java.util.HashMap;
 
@@ -38,17 +33,14 @@ public class MainActivity extends AppCompatActivity {
     setContentView(R.layout.activity_main);
     context = MainActivity.this;
 
-    new UserLoginRequest(context).execute();
+//    new UserLoginRequest(context).execute();
 
     initViews();
   }
 
   private void initViews() {
-    final TextInputEditText carNbEt = findViewById(R.id.et_car_number);
-
-    FrameLayout carLtrLayout = findViewById(R.id.layout_car_letter);
-    carLtrLayout.setBackground(GeneralUtils.getStroke(context, 4, 1));
-    carLtrLayout.setOnClickListener(new View.OnClickListener() {
+    AppCompatTextView carLetterTv = findViewById(R.id.tv_car_letter);
+    carLetterTv.setOnClickListener(new View.OnClickListener() {
       @Override
       public void onClick(View view) {
         PopupMenu popup = new PopupMenu(context, view);
@@ -69,61 +61,64 @@ public class MainActivity extends AppCompatActivity {
     submitBtn.setOnClickListener(new View.OnClickListener() {
       @Override
       public void onClick(View view) {
-        tryDoNextCall(context, carNbEt.getText().toString(), carLtr);
+        AppCompatEditText carNbEt = findViewById(R.id.et_car_number);
+        tryDoNextCall(context, carNbEt.getText().toString(), getArabicCarLetter(carLtr));
       }
     });
   }
 
+  private String getArabicCarLetter(String arabicCarLetter) {
+    switch (arabicCarLetter) {
+      case "#":
+        return "#";
+      case "*":
+        return "*";
+      case "--":
+        return "--";
+      case "B":
+        return "ب";
+      case "G":
+        return "ج";
+      case "D":
+        return "د";
+      case "R":
+        return "ر";
+      case "Z":
+        return "ز";
+      case "S":
+        return "ص";
+      case "T":
+        return "ط";
+      case "A":
+        return "ع"; //TODO: ع is not A
+      case "K":
+        return "ق"; //TODO: Not sure if ق is K
+      case "M":
+        return "م";
+      case "N":
+        return "ن";
+      case "O":
+        return "و";
+    }
+    return "#";
+  }
+
   private static class UserLoginRequest extends AsyncTask<Void, Void, String> {
-    Context context;
-    String response = "";
-
-    UserLoginRequest(Context context) {
-      this.context = context;
-    }
-
-    public String doInBackground(Void... voidArr) {
-      response = new HttpServiceHandler().makeServiceCall("http://lebanonride.com/and1.0/apns.php?task=register&deviceuid=" + GeneralUtils.getDeviceUId());
-      return response;
-    }
-  }
-
-  private void tryDoNextCall(final Context context, final String carNb, final String carLtr) {
-    new UserRewardRequest(context, new RequestCallback() {
-      @Override
-      public void onSuccess() {
-        new CarInfoRequest(context, carNb, carLtr).execute();
-      }
-
-      @Override
-      public void onFailure() {
-        Toast.makeText(context, "Please make sure you're connected to the internet", Toast.LENGTH_SHORT).show();
-      }
-    }).execute();
-  }
-
-  private class UserRewardRequest extends AsyncTask<Void, Void, String> {
     Context context;
     RequestCallback callback;
     String response = "";
 
-    UserRewardRequest(Context context, RequestCallback requestCallback) {
+    UserLoginRequest(Context context, RequestCallback requestCallback) {
       this.context = context;
       callback = requestCallback;
     }
 
     public String doInBackground(Void... voidArr) {
-      if(!GeneralUtils.isNetworkAvailable(context)) {
+      if (!GeneralUtils.isNetworkAvailable(context)) {
         callback.onFailure();
         return null;
       }
-
-      HashMap<String, String> hashMap = new HashMap();
-      long currentTimeMillis = System.currentTimeMillis() / 1000;
-      currentTimeMillis = ((currentTimeMillis * 2) - 1000) / 4;
-      hashMap.put("X", String.valueOf(currentTimeMillis));
-      hashMap.put("Y", GeneralUtils.getDeviceUId());
-      response = new HttpServiceHandler().makeServiceCallPost(context, "http://lebanonride.com/and1.0/reward.php", hashMap, true);
+      response = new HttpServiceHandler().makeServiceCall("http://lebanonride.com/and1.0/apns.php?task=register&deviceuid=" + GeneralUtils.getDeviceUId(context));
       return response;
     }
 
@@ -132,6 +127,68 @@ public class MainActivity extends AppCompatActivity {
       callback.onSuccess();
     }
   }
+
+  private void tryDoNextCall(final Context context, final String carNb, final String carLtr) {
+    if (GeneralUtils.getUsedCount(context) >= 5) {
+      GeneralUtils.resetUsedCount(context);
+      GeneralUtils.generateAccount(context);
+      new UserLoginRequest(context, new RequestCallback() {
+        @Override
+        public void onSuccess() {
+          new CarInfoRequest(context, carNb, carLtr).execute();
+        }
+
+        @Override
+        public void onFailure() {
+          Toast.makeText(context, "Please make sure you're connected to the internet", Toast.LENGTH_SHORT).show();
+        }
+      }).execute();
+    } else {
+      new CarInfoRequest(context, carNb, carLtr).execute();
+    }
+//    new UserRewardRequest(context, new RequestCallback() {
+//      @Override
+//      public void onSuccess() {
+//        new CarInfoRequest(context, carNb, carLtr).execute();
+//      }
+//
+//      @Override
+//      public void onFailure() {
+//        Toast.makeText(context, "Please make sure you're connected to the internet", Toast.LENGTH_SHORT).show();
+//      }
+//    }).execute();
+  }
+
+//  private class UserRewardRequest extends AsyncTask<Void, Void, String> {
+//    Context context;
+//    RequestCallback callback;
+//    String response = "";
+//
+//    UserRewardRequest(Context context, RequestCallback requestCallback) {
+//      this.context = context;
+//      callback = requestCallback;
+//    }
+//
+//    public String doInBackground(Void... voidArr) {
+//      if(!GeneralUtils.isNetworkAvailable(context)) {
+//        callback.onFailure();
+//        return null;
+//      }
+//
+//      HashMap<String, String> hashMap = new HashMap();
+//      long currentTimeMillis = System.currentTimeMillis() / 1000;
+//      currentTimeMillis = ((currentTimeMillis * 2) - 1000) / 4;
+//      hashMap.put("X", String.valueOf(currentTimeMillis));
+//      hashMap.put("Y", GeneralUtils.getDeviceUId(context));
+//      response = new HttpServiceHandler().makeServiceCallPost(context, "http://lebanonride.com/and1.0/reward.php", hashMap, true);
+//      return response;
+//    }
+//
+//    public void onPostExecute(String str) {
+//      super.onPostExecute(str);
+//      callback.onSuccess();
+//    }
+//  }
 
   private class CarInfoRequest extends AsyncTask<Void, Void, String> {
     Context context;
@@ -145,20 +202,30 @@ public class MainActivity extends AppCompatActivity {
 
     public String doInBackground(Void... voidArr) {
       HashMap<String, String> hashMap = new HashMap<>();
-      hashMap.put("deviceuid", GeneralUtils.getDeviceUId());
+      hashMap.put("deviceuid", GeneralUtils.getDeviceUId(context));
       hashMap.put("number", carNb);
       hashMap.put("letter", carLtr);
       hashMap.put("submitBtn", "");
 
-      return new HttpServiceHandler().doSubmit("http://lebanonride.com/and1.0/", hashMap);
+      return new HttpServiceHandler().doSubmit("http://lebanonride.com/and1.0/?deviceuid=" + GeneralUtils.getDeviceUId(context), hashMap);
     }
 
     public void onPostExecute(final String str) {
       super.onPostExecute(str);
 
+      GeneralUtils.incrementUsedCount(context);
+
+      if (str.split("<div class=\"col-md-6 col-md-offset-3\">")[1].contains("alert-danger")) {
+        new MaterialAlertDialogBuilder(context)
+            .setMessage("No results")
+            .setCancelable(false)
+            .setNeutralButton("Ok", null)
+            .show();
+        return;
+      }
+
       WebView webView = new WebView(context);
       webView.loadData(str.split("<div class=\"col-md-6 col-md-offset-3\">")[1].split("<hr>")[1], "text/html", "utf-8");
-
       new MaterialAlertDialogBuilder(context)
           .setView(webView)
           .setCancelable(false)
@@ -169,6 +236,7 @@ public class MainActivity extends AppCompatActivity {
 
   private interface RequestCallback {
     void onSuccess();
+
     void onFailure();
   }
 }
--- app/src/main/java/com/batache/cars/Utils/GeneralUtils.java
+++ app/src/main/java/com/batache/cars/Utils/GeneralUtils.java
@@ -1,19 +1,27 @@
 package com.batache.cars.Utils;
 
 import android.content.Context;
+import android.content.SharedPreferences;
 import android.content.res.ColorStateList;
 import android.graphics.Color;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
+import android.preference.PreferenceManager;
 
 import androidx.core.app.NotificationManagerCompat;
 
 import com.batache.cars.R;
 import com.google.android.material.shape.MaterialShapeDrawable;
 
+import java.util.Random;
+
 public class GeneralUtils {
-  public static String getDeviceUId() {
-    return "cars_app";
+  private static final String ACCOUNT_NUMBER = "account_number";
+  private static final int ACCOUNT_NUMBER_MAX_LENGTH = 10;
+  private static final String ACCOUNT_USED_COUNT = "account_used_count";
+
+  public static String getDeviceUId(Context context) {
+    return GeneralUtils.getAccount(context);
   }
 
   public static boolean isNetworkAvailable(Context context) {
@@ -25,15 +33,55 @@ public class GeneralUtils {
     return ((ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE)).getActiveNetworkInfo();
   }
 
-  public static MaterialShapeDrawable getStroke(Context context, int cornerRadius, int strokeWidth) {
-    MaterialShapeDrawable stroke = new MaterialShapeDrawable();
-    stroke.setTintList(ColorStateList.valueOf(Color.TRANSPARENT));
-    stroke.setCornerRadius(convertDpToPixel(context, cornerRadius));
-    stroke.setStroke(convertDpToPixel(context, strokeWidth), ColorStateList.valueOf(context.getResources().getColor(R.color.fieldStrokeColor)));
-    return stroke;
+  public static String generateAccount(Context context) {
+    SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(context);
+    SharedPreferences.Editor editor = preferences.edit();
+    String random = random();
+    editor.putString(ACCOUNT_NUMBER, random);
+    editor.apply();
+    return random;
   }
 
-  private static float convertDpToPixel(Context context, float dp) {
-    return dp * context.getResources().getDisplayMetrics().density;
+  public static String random() {
+    String DATA = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    Random RANDOM = new Random();
+
+    StringBuilder sb = new StringBuilder(ACCOUNT_NUMBER_MAX_LENGTH);
+
+    for (int i = 0; i < ACCOUNT_NUMBER_MAX_LENGTH; i++) {
+      sb.append(DATA.charAt(RANDOM.nextInt(DATA.length())));
+    }
+
+    return sb.toString();
+  }
+
+  public static String getAccount(Context context) {
+    String account = PreferenceManager.getDefaultSharedPreferences(context).getString(ACCOUNT_NUMBER, "generate");
+
+    if(account.equals("generate")) {
+      return generateAccount(context);
+    }
+
+    return account;
+  }
+
+  public static int incrementUsedCount(Context context) {
+    SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(context);
+    SharedPreferences.Editor editor = preferences.edit();
+    int count = getUsedCount(context) + 1;
+    editor.putInt(ACCOUNT_USED_COUNT, count);
+    editor.apply();
+    return count;
+  }
+
+  public static int getUsedCount(Context context) {
+    return PreferenceManager.getDefaultSharedPreferences(context).getInt(ACCOUNT_USED_COUNT, 5);
+  }
+
+  public static void resetUsedCount(Context context) {
+    SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(context);
+    SharedPreferences.Editor editor = preferences.edit();
+    editor.putInt(ACCOUNT_USED_COUNT, 0);
+    editor.apply();
   }
 }
